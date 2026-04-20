e95a1eb1068c1d0bd8630d9c7269f165148d8c07
bema <d00230369@student.dkit.ie>
2026-04-20 20:45:46 +0100

    model files

---
+++ WebSharingApp/Model/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace WebSharingApp.Model
+{
+    public class Item
+    {
+        public int itemId { get; set; }
+        public string itemName { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public string condition { get; set; }
+        public string image {  get; set; }
+        public DateTime DatePosted { get; set; }
+
+
+
+    }
---
+++ WebSharingApp/Model/Request.cs
@@ -0,0 +1,11 @@
+﻿namespace WebSharingApp.Model
+{
+    public class Request
+    {
+        public int requestId { get; set; }
+        public int itemId { get; set; }
+        public string requesterId { get; set; }
+        public string status { get; set; }
+        public DateTime requestDate { get; set; }
+    }
+}
